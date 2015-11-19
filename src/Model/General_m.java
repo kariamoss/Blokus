9f5bb06415f21b832fd9bec8e3335833fc8d6b74
kariamoss <jehanportable@gmail.com>
2015-11-19 18:01:29 +0100

    Ajout d'inventaires au model général

--- src/Model/General_m.java
+++ src/Model/General_m.java
@@ -2,5 +2,17 @@ package Model;
 
 
 public class General_m {
-    Inventaire_m pieceM;
+    Inventaire_m inventaireRouge;
+    Inventaire_m inventaireJaune;
+    Inventaire_m inventaireVert;
+    Inventaire_m inventaireBleu;
+
+    public General_m(){
+        inventaireBleu = new Inventaire_m("Bleu");
+        inventaireRouge = new Inventaire_m("Rouge");
+        inventaireJaune = new Inventaire_m("Jaune");
+        inventaireVert = new Inventaire_m("Vert");
+    }
 }
+
+
