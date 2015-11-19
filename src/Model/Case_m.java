d379dbdd2e20995ead64e931cc028c1a00d1f09b
kariamoss <jehanportable@gmail.com>
2015-11-19 16:39:05 +0100

    Création du model case

--- src/Model/Case_m.java
+++ src/Model/Case_m.java
@@ -2,4 +2,38 @@ package Model;
 
 
 public class Case_m {
+    String couleur;
+    int[] pos = new int[2];
+
+    public Case_m(String couleur, int i, int j){
+        this.couleur = couleur;
+        pos[0] = i;
+        pos[1] = j;
+    }
+
+
+
+
+    //Getters Setters
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public int[] getPos() {
+        return pos;
+    }
+
+    public void setPos(int[] pos) {
+        this.pos = pos;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+
+
+
 }
+
+
