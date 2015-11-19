1075b86f83f2ad05a90716dab1d41cbfce1926ef
Fox Mc-Tag <foxmctag@gmail.com>
2015-11-20 00:44:31 +0100

    composant graphique Grille_v à utiliser pour l'assemblage de la vue de l'application.

---
+++ src/Vue/Grille_v.java
@@ -0,0 +1,63 @@
+package Vue;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+/**
+ * Created by Fox Mc-Tag on 19/11/2015.
+ * Property of Mc-Tag's corporation, all rights reserved.
+ */
+public class Grille_v {
+    JPanel grille;
+
+
+
+
+    public Grille_v(){
+        initGrid(600/3, 20, true);
+    }
+
+    public Grille_v(int sizeInPixels, int nbButtonsPerRow, boolean borders){
+        initGrid(sizeInPixels, nbButtonsPerRow, borders);
+    }
+
+
+
+    private void initGrid(int sizeInPixels, int nbButtonsPerRow, boolean borders){
+
+        grille= new JPanel( new GridLayout(nbButtonsPerRow,nbButtonsPerRow));
+        grille.setPreferredSize(new Dimension(sizeInPixels, sizeInPixels));
+        grille.setOpaque(false);
+
+        fillGrid(nbButtonsPerRow, borders);
+
+    }
+
+
+
+    public JPanel getGrille(){ return grille; }
+
+
+    private void fillGrid(int nbButtonsPerRow, boolean borders){
+        int nbButtons= nbButtonsPerRow*nbButtonsPerRow;
+
+        for (int i= 0; i < nbButtons; i++){
+            JButton button= new JButton();
+
+            if (borders){
+                button.setBorder(new LineBorder(Color.DARK_GRAY,1));
+            }
+            else {
+                button.setBorder(new LineBorder(Color.DARK_GRAY,0));
+            }
+            grille.add(button);
+
+        }
+
+    }
+
+
+
+
+}
