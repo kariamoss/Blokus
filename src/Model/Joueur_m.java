b60c0485e5c1c37ad5e8bc245d7bdc35ab7763ab
kariamoss <jehanportable@gmail.com>
2015-11-26 20:32:27 +0100

    Ajout du model de joueur

---
+++ src/Model/Joueur_m.java
@@ -0,0 +1,78 @@
+package Model;
+
+import sun.font.TrueTypeFont;
+
+/**
+ * Created by Jehan on 26/11/2015.
+ */
+public class Joueur_m {
+    Inventaire_m inventaire;
+    String couleur;
+    boolean enJeu;
+    boolean tourDeJeu;
+    int score;
+    String nom;
+
+    public Joueur_m(String couleur, String nom){
+        this.couleur = couleur;
+        inventaire = new Inventaire_m(couleur);
+        this.nom = nom;
+        enJeu = true;
+        tourDeJeu = false;
+        score = 0;
+    }
+
+
+
+
+    // GETTERS / SETTERS
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Inventaire_m getInventaire() {
+        return inventaire;
+    }
+
+    public void setInventaire(Inventaire_m inventaire) {
+        this.inventaire = inventaire;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public boolean isEnJeu() {
+        return enJeu;
+    }
+
+    public void setEnJeu(boolean enJeu) {
+        this.enJeu = enJeu;
+    }
+
+    public boolean isTourDeJeu() {
+        return tourDeJeu;
+    }
+
+    public void setTourDeJeu(boolean tourDeJeu) {
+        this.tourDeJeu = tourDeJeu;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
+
