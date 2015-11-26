aa91a905a2fd7419a495a349c6c8ce4b01b322f0
kariamoss <jehanportable@gmail.com>
2015-11-26 15:07:31 +0100

    Git conflit résolu

--- src/Vue/General_v.java
+++ src/Vue/General_v.java
@@ -5,9 +5,9 @@ import Model.General_m;
 import javax.swing.*;
 import java.awt.*;
 
-/**
- * Created by Jehan Milleret on 29/04/2015.
- */
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+
 public class General_v extends JFrame {
     protected General_m model;
     protected BoutonsControleJeu_v fenetreControle = new BoutonsControleJeu_v();
@@ -28,15 +28,11 @@ public class General_v extends JFrame {
     public void initAttribute() {
         fenetre = new JPanel();
         fenetre.setBackground(Color.GRAY);
-<<<<<<< HEAD
-        fenetre.add(new Grille_v().getGrille());
+        fenetre.add(new Grille_v(550, 20, true).getGrille());
         fenetre.add(fenetreControle.getPanelDeControle());
 
-=======
-        fenetre.add(new Grille_v(550, 20, true).getGrille());
         Inventaire_v inventaire = new Inventaire_v();
         fenetre.add(inventaire.getInventaire());
->>>>>>> 508ffa460ef71f2c39f260a51edcee58a1ce4489
     }
 
     public void display() {
