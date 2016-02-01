353dd66393d06bee6cac7e36c4512349ceb0abfe
pachobo16 <pierratlucas@gmail.com>
2016-02-01 23:47:18 +0100

    Changement ControlAbandonner

--- src/Controler/ControlAbandonner.java
+++ src/Controler/ControlAbandonner.java
@@ -7,8 +7,6 @@ import Vue.General_v;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,7 +25,7 @@ public class ControlAbandonner {
         //vueGeneral.boutonsControleJeu.setAbandonButtonControler(this);
     }
 
-    public void abandon(){
+    public void abandon() {
         joueur = modelGeneral.selectJoueurActif();
 
         //Message de dialogue
@@ -44,63 +42,56 @@ public class ControlAbandonner {
         vueGeneral.overview.overviewButton.setBorderPainted(false);
 
 
-
-
-
-        if (modelGeneral.getNbJoueuEnJeu()>0){
+        if (modelGeneral.getNbJoueuEnJeu() > 0) {
             //On passe au joueur suivant
             modelGeneral.joueurSuivant();
 
             //On change d'inventaire
             List<Piece_m> listPiece = modelGeneral.selectJoueurActif().getInventaire().getListPiece();
 
-            for (int i =0;i<listPiece.size()-1;i++)
-            {
+            for (int i = 0; i < listPiece.size() - 1; i++) {
                 vueGeneral.inventaire.tabButtonInventaire[i].setEnabled(true);
-                if(modelGeneral.selectJoueurActif().getInventaire().getPiece(i).isUsed())
-                {
+                if (modelGeneral.selectJoueurActif().getInventaire().getPiece(i).isUsed()) {
                     vueGeneral.inventaire.tabButtonInventaire[i].setEnabled(false);
                 }
                 ImageIcon imageIcon = new ImageIcon(listPiece.get(i).getImage());
                 Image image = imageIcon.getImage();
-                Image newImage = image.getScaledInstance(45, 45, java.awt.Image.SCALE_SMOOTH) ;
+                Image newImage = image.getScaledInstance(45, 45, java.awt.Image.SCALE_SMOOTH);
                 ImageIcon icon = new ImageIcon(newImage);
 
                 vueGeneral.inventaire.tabButtonInventaire[i].setIcon(icon);
             }
-        }
-        else
-        {
+        } else {
             finDePartie();
         }
     }
 
 
-    private void finDePartie(){
+    private void finDePartie() {
 
         //Joueur_m vainqueur = modelGeneral.selectJoueurActif();
         String tabNom[] = new String[4];
         int tabScore[] = new int[4];
 
         //Tri des joueur :
-        for (int i = 0;i<4;i++)
-        {
+        for (int i = 0; i < 4; i++) {
             Joueur_m joueur = modelGeneral.getJoueur(i);
             tabScore[i] = joueur.getScore();
             tabNom[i] = joueur.getNom();
         }
 
+
         Arrays.sort(tabScore);
 
         String result = "Fin de partie\n\n";
-        for (int i=3;i>=0;i--){
-            result+=modelGeneral.getJoueurByScore(tabScore[i]).getNom() +  " : " + tabScore[i] + "pts\n";
-            System.out.println(modelGeneral.getJoueurByScore(tabScore[i]).getNom() +  " : " + tabScore[i] + "pts");
+        for (int i = 3; i >= 0; i--) {
+            result += modelGeneral.getJoueurByScore(tabScore[i]).getNom() + " : " + tabScore[i] + "pts\n";
+            System.out.println(modelGeneral.getJoueurByScore(tabScore[i]).getNom() + " : " + tabScore[i] + "pts");
         }
 
-        JOptionPane victoire = new JOptionPane();
-        JOptionPane.showMessageDialog(null, result, "Fin de la partie", JOptionPane.INFORMATION_MESSAGE);
+        vueGeneral.informationMessage(result, "Fin de partie");
 
         System.exit(0);
     }
+
 }
